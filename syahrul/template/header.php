e12967e401ac1fcd45302f2601f5d6dcfe928407
Muhammad Syahrul Romadhon <135720528+MuhSyahrul6@users.noreply.github.com>
2023-09-05 00:47:40 +0700

    Add files via upload

---
+++ syahrul/template/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Toko Syahrul</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="rulzcss/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="rulzcss/css/sb-admin-2.min.css" rel="stylesheet">
+    <!-- DATATABLES BS 4-->
+    <link rel="stylesheet" href="rulzcss/vendor/datatables/dataTables.bootstrap4.css" />
+    <!-- Bootstrap core JavaScript-->
+    <script src="rulzcss/vendor/jquery/jquery.min.js"></script>
+    <script src="rulzcss/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- Core plugin JavaScript-->
+    <script src="rulzcss/vendor/jquery-easing/jquery.easing.min.js"></script>
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        
